38aeaa6dae70c507d68425947a8d0a5a8a38a2ab
JosephS068 <43795572+JosephS068@users.noreply.github.com>
2019-11-03 15:10:45 +0000

    Adding Files

---
+++ CANTest.ino
@@ -0,0 +1,98 @@
+// demo: CAN-BUS Shield, send data
+// loovee@seeed.cc
+
+#include <shield_mcp_can.h>
+#include <SPI.h>
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 10;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    Serial.println("CAN BUS Shield init ok!");
+}
+
+unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int randomNumber;
+
+void loop()
+{
+    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
+//
+//    stmp[0] = 2;
+//    stmp[1] = 5;
+//    stmp[2] = 2;
+//    stmp[3] = 5;
+//    stmp[4] = 2;
+//    stmp[5] = 5;
+//    stmp[6] = 2;
+//    stmp[7] = 5;
+
+    //Shift Light tests
+    if(stmp[6] == 1){
+      stmp[0] = 0;
+      stmp[2] = 0;
+      stmp[4] = 0;
+      stmp[6] = 0;
+    }
+    else{
+      if(stmp[2] == 0){
+        stmp[2] = 1;
+      }
+      else{
+        if(stmp[4] == 0){
+          stmp[4] = 1;
+        }
+        else{
+          stmp[6] = 1;
+        }
+      }
+    }
+
+    Serial.print("1: ");
+    Serial.print(stmp[0], DEC);
+
+    Serial.print(" 2: ");
+    Serial.print(stmp[1], DEC);
+    
+    Serial.print(" 3: ");
+    Serial.print(stmp[2], DEC);
+
+    Serial.print(" 4: ");
+    Serial.print(stmp[3], DEC);
+
+    Serial.print(" 5: ");
+    Serial.print(stmp[4], DEC);
+
+    Serial.print(" 6: ");
+    Serial.print(stmp[5], DEC);
+
+    Serial.print(" 7: ");
+    Serial.print(stmp[6], DEC);
+
+    Serial.print(" 8: ");
+    Serial.println(stmp[7], DEC);
+
+    
+    CAN.sendMsgBuf(0x2EC, 0, 8, stmp);
+    delay(100);
+    CAN.sendMsgBuf(0x2ED, 0, 8, stmp);
+    delay(100);
+    CAN.sendMsgBuf(0x2EE, 0, 8, stmp);
+    delay(100);
+    CAN.sendMsgBuf(0x2EF, 0, 8, stmp);
+    delay(1000);                       // send data per 1000ms
+}
+
+// END FILE
---
+++ DisplayDriver.ino
@@ -0,0 +1,118 @@
+/* Takes 8 bytes and an ID from the can network. Checks ID then processes information accordingly.
+ * Formats information returns the created String from data and an ID to match up with the UI's IDs.
+ */
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include <Nextion.h>
+#include <shield_mcp_can.h>
+
+SoftwareSerial nextion(2, 3);// nextion TX to pin 2 and RX to pin 3 of Arduino
+
+Nextion my_nextion(nextion, 9600); //create a nextion object named my_nextion using the nextion serial port @ 9600bps
+
+const int SPI_CS_PIn = 10;
+
+String data;
+String num;
+char lastChar;
+unsigned int value;
+
+MCP_CAN CAN(SPI_CS_PIn);                                    // Set CS pin
+
+void setup() {
+  Serial.begin(115200);
+  while (CAN_OK != CAN.begin(CAN_1000KBPS))              // init can bus : baudrate = 500k
+  {
+      Serial.println("CAN BUS Shield init fail");
+      Serial.println(" Init CAN BUS Shield again");
+      delay(100);
+  }
+  Serial.println("CAN BUS Shield init ok!");
+  my_nextion.init();
+}
+
+void loop()
+{
+  unsigned char len = 0;
+    unsigned char buf[8];
+    // Serial.println(CAN.checkReceive());
+
+    if(CAN_MSGAVAIL == CAN.checkReceive())            // check if data coming
+    {
+        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+        //Serial.println("Received");
+        unsigned int canId = CAN.getCanId();
+        char* command = "USL.pic=#";
+        switch(canId){
+          //Data for RPM, Engine Temperature, Oil Temperature and Oil Pressure
+          case 0x2EC:
+            //RPM
+            value = (buf[0]<<8) + buf[1]; 
+            data = (String)value;
+            my_nextion.setComponentText("RPM", data); 
+
+            // Engine Temperature, We are of by a power of ten, during string creation we fix this issue
+            value = (buf[2]<<8) + buf[3];
+            data = value;
+            lastChar = data.charAt(data.length()-1);
+            data.setCharAt(data.length()-1, '.');
+            data += lastChar;
+            my_nextion.setComponentText("ETM", data); 
+
+            // Oil Temperature, Same as Engine Temperature
+            value = (buf[4]<<8) + buf[5];
+            data = value;
+            lastChar = data.charAt(data.length()-1);
+            data.setCharAt(data.length()-1, '.');
+            data += lastChar;
+            my_nextion.setComponentText("OTM", data); 
+
+            // Oil Pressure, Multiplied by 145 to get proper value
+            value = (buf[6]<<8) + buf[7];
+            value*=145;
+            data = value;//changed num to data
+            data = data.substring(0, data.length()-3) + "." + data.charAt(data.length()-3);
+            my_nextion.setComponentText("OPR", data); 
+          break;
+
+          //Data for Lap Delta, Lateral Acceleration and Ground Speed
+          case 0x2ED:
+            // Lap Delta
+            value = (buf[0]<<8)+buf[1];
+            data = (String)value; //Changed num to data
+            data = data.substring(0, data.length()-3) + "`" + data.substring(data.length()-3, data.length()-1);
+            my_nextion.setComponentText("LDT", data);
+             
+            // Lateral Acceleration
+            value = (buf[2]<<8) + buf[3];
+            data = (String)value;
+            my_nextion.setComponentText("LAC", data); 
+
+            // Ground Speed
+            value = (buf[4]<<8) + buf[5];
+            data = (String)value;
+            my_nextion.setComponentText("GSP", data); 
+          break;
+
+          //Data for Up Shift Light
+          case 0x2EE:
+            value = 0;
+            if(buf[0] || buf[1]) value++;
+            if(buf[2] || buf[3]) value++;
+            if(buf[4] || buf[5]) value++;
+            if(buf[6] || buf[7]) value++;
+            Serial.println("USL.pic=" + (String)value);
+           command[8]=value+'0';
+            my_nextion.sendCommand(command); 
+            my_nextion.sendCommand("ref USL");  
+          break;
+
+          //Data for Gear Position
+          case 0x2EF:
+            if(buf[0] || buf[1]) data = "N";
+            else data = (String)((buf[2]<<8) + buf[3]);
+            my_nextion.setComponentText("GEAR", data);
+          break;
+        }
+    }
+}
